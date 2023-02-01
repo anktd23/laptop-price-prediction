c45f66cd4f5b84e6a934ac6e0feee46fc989d9c1
Aniket Dumbre <anktd23@gmail.com>
2023-02-01 22:18:00 +0530

    web scraping done

---
+++ web-scraping/scraper.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+s = Service('"F:\chromedriver.exe"')
+
+driver = webdriver.Chrome(service = s)
+
+driver.get('https://www.smartprix.com/laptops')
+time.sleep(1)
+
+driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[4]/span').click()
+time.sleep(1)
+
+old_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+
+    driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+    time.sleep(1)
+
+    new_height = driver.execute_script('return document.body.scrollHeight')
+
+    print(old_height)
+    print(new_height)
+
+    if new_height == old_height:
+        break
+
+    old_height = new_height
+
+html = driver.page_source
+
+with open('laptop.html','w',encoding='utf-8') as f:
+    f.write(html)
+
+
