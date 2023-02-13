4726a7a625046c63a42095bec9bf8b1848742db1
Aniket Dumbre <anktd23@gmail.com>
2023-02-13 13:37:26 +0530

    data_dump.py updated

--- data_dump.py
+++ data_dump.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     df.reset_index(drop=True,inplace=True)
     json_record = list((json.loads(df.T.to_json()).values()))
     print(json_record[0])
-
+    
     mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
