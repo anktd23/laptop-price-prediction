936865262044a1ee2963ea07465c7d54cefb1064
Aniket Dumbre <anktd23@gmail.com>
2023-02-16 14:27:38 +0530

    data_validation.py updated

--- predictor/components/data_validation.py
+++ predictor/components/data_validation.py
@@ -64,7 +64,7 @@ class DataValidation:
             missing_columns = []
             for base_column in base_columns:
                 if base_column not in current_columns:
-                    logging.info(f"Column: [{base} is not available.]")
+                    logging.info(f"Column: [{base_column} is not available.]")
                     missing_columns.append(base_column)
 
             if len(missing_columns)>0:
